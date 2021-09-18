298675acf0943021719039c41fc699e84f2172c7
Fabien Winkler <fabien.winkler@outlook.fr>
2021-09-18 14:06:49 +0200

    [Fix] syntaxe

--- server/index.js
+++ server/index.js
@@ -1,27 +1,26 @@
 require('dotenv').config();
 
-import express, { urlencoded, json } from 'express';
-import { verifyToken } from './middleware/authJwt';
-import helmet from 'helmet';
+const express = require('express');
+const { verifyToken } = require('./middleware/authJwt');
+const helmet = require('helmet');
 const port = 3000;
 const app = express();
-import cors from 'cors';
-import send from './mailer';
-
+const cors = require('cors');
+const send = require('./mailer');
 // Security
 app.use(helmet());
 
 app.use(cors());
-app.use(urlencoded({ extended: true }));
-app.use(json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(
-    json({
+    express.json({
         type: ['application/json', 'text/plain']
     })
 );
 
 // *Models
-import { sequelize } from './models';
+const db = require('./models');
 
 // Routes
 app.get('/api/checkToken', verifyToken, (req, res) => {
@@ -37,7 +36,7 @@ app.post('/api/contact', (req, res) => {
 require('./routes/user.routes')(app);
 require('./routes/service.routes')(app);
 
-sequelize.sync({ alter: false }).then(async () => {
+db.sequelize.sync({ alter: false }).then(async () => {
     app.listen(port, () => {
         console.info(`API run at http://localhost:${port}`);
     });
