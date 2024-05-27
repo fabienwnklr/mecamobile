295046a4b6a0e66b0899cb75f3e23bca537379b9
fabienwnklr <fabien.winkler@outlook.fr>
2024-05-27 14:43:49 +0200

    ci:  fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -12,11 +12,10 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
+git init --initial-branch=master
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:fabienwnklr/mecamobile-prod.git master
+git push -f https://github.com/fabienwnklr/mecamobile-prod master
 
 cd -
