1cd971ee0a9062f7c3907f428b78ca2b0a4ef0a0
fabienwnklr <fabien.winkler@outlook.fr>
2024-05-27 18:08:30 +0200

    fix: add login username info

--- src/components/admin/BackPrevRoute.vue
+++ src/components/admin/BackPrevRoute.vue
@@ -1,5 +1,27 @@
 <template>
-    <v-btn title="Retour" fab absolute left small @click="$router.go(-1)">
-        <v-icon>mdi-arrow-left</v-icon>
-    </v-btn>
+    <div>
+        <v-btn title="Retour" fab absolute left small @click="$router.go(-1)">
+            <v-icon>mdi-arrow-left</v-icon>
+        </v-btn>
+        <h2 class="mb-2">
+            Connecté en tant que :
+            <b>
+                {{ this.username }}
+            </b>
+        </h2>
+    </div>
 </template>
+
+<script>
+export default {
+    name: 'BackPrevRoute',
+    data: () => ({
+        username: ''
+    }),
+    mounted() {
+        const user = localStorage.getItem('user');
+
+        if (user) this.username = JSON.parse(user).username;
+    }
+};
+</script>
